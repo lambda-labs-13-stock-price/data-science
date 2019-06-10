649f3b09215115be9602ee16f80777993386effe
Cole Hudson <cole@colejhudson.com>
2019-06-10 12:58:37 -0700

    added notes

--- main.go
+++ main.go
@@ -4,17 +4,16 @@ import (
   "os"
 
   "./workers"
-
-  "github.com/aws/aws-sdk-go/aws"
-  "github.com/aws/aws-sdk-go/aws/session"
-  "github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/sqs"
 )
 
 func main() {
 	workerCount := 20
 	workerQueue := make(chan *workers.Worker, workerCount)
 
+  /*
+    Instantiating worker goroutines to do work as they
+    become ready.
+  */
 	for i := 0; i < workerCount; i++ {
 		worker := &workers.Worker{
 			Id: i,
@@ -27,15 +26,24 @@ func main() {
 		workerQueue <- worker
 	}
 
-  for todo := range Todos {
-    worker := <-workerQueue
+  /*
+    When a task is available for a worker to do, we parse it
+    into a standard format to determine what kind of worker
+    should undertake the given task.
 
+    Having made that decision we construct the appropriate
+    context for that worker, wait for a worker to become
+    available from the worker queue, and wait again for
+    tasks to become available.
+  */
+  for todo := range Todos {
     job := &worker.Job{
       Kind: "Crawl"
       Info: &CrawlingJob{
       }
     }
 
+    worker := <-workerQueue
     worker.JobQueue <- job
   }
 }
