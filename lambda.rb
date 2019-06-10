7313337a30d2944a990af6930886d841e08cd73a
Cole Hudson <cole@colejhudson.com>
2019-06-10 02:37:11 -0700

    fixed function parameter binding

--- lambda.rb
+++ lambda.rb
@@ -1,7 +1,7 @@
 require 'aws-sdk-sqs'
 require 'json'
 
-def handle(event, context)
+def handle(event:, context:)
   sqs = Aws::SQS::Client.new(region: ENV['AWS_REGION_CODE'])
   message = 'success'
   status = 200
@@ -9,7 +9,7 @@ def handle(event, context)
     "Content-Type" => "application/json"
   }
 
-  if event["method"] == "POST"
+  if event["httpMethod"] == "POST"
     sqs.send_message(queue_url: ENV['AWS_SQS_URL'], message_body: event.to_json )
   else
     status = 400
