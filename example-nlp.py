46ce8e2d15ea0887d127915f8c30dd433e1151f1
macscheffer <43249742+macscheffer@users.noreply.github.com>
2019-06-11 13:10:21 -0400

    Delete example-nlp.py

--- example-nlp.py
+++
@@ -1,2 +0,0 @@
-lexicon = dict(zip(pd.read_csv('Downloads/vader_lexicon.csv', encoding='cp437', header=None)[[0, 1]].values.T[0], pd.read_csv('Downloads/vader_lexicon.csv', encoding='cp437', header=None)[[0, 1]].values.T[1]))
-s = Sentiment(lexicon=lexicon)
