d130dcb4f601d959fd452827f1dbc846ccbb5270
Ruwai <chrisl6uie@gmail.com>
2019-06-12 07:33:33 -0700

    Created new branch for RDS-functions in a branch called RDS-pipeline. commits to this branch will follow with RDS functionality and automation to move towards homebrewed ETL jobs

---
+++ RDS-functions/README.md
@@ -0,0 +1,10 @@
+# RDS
+
+In this repo all of the RDS related Lambda functions will reside here
+
+- there is a repo for psycopg2 solution and SQLAlchemy solution.
+- To be honest I do not know how effective it is for me to have created solutions with different methods, but time will tell. 
+
+**DISCLAIMER**
+- Files in this repository are not intended to be tested locally or on any test environment, just with AWS tools and services.
+- Unless specified, there are `os.environ` environment variables that need to be set before usage. 
---
+++ RDS-functions/SQLAlchemy/create_tables.py
@@ -0,0 +1,26 @@
+from sqlalchemy import MetaData, Table, Column, String, Integer
+from utils import ENGINE, CreateTable, CreateTweetsTable
+
+METADATA = MetaData()
+METADATA.reflect(bind=ENGINE)
+
+def create_table(name, METADATA):
+	'''
+		Create a new table with a specified name.
+		:name: Name of table (i.e. donald_trump_tweets, nancy_pelosi_tweets)
+		:metadata: Specified metadata 
+	'''
+	tables = METADATA.tables.keys()
+	if name not in tables: 
+		table = CreateTable()
+		table.create(engine)
+
+# Probably will be deprecated 
+def create_tweets_table(name, METADATA):
+	'''
+		To create initial tweets table
+	'''
+	tables = METADATA.tables.keys()
+	if name not in tables:
+		table = CreateTweetsTable()
+		table.create(ENGINE)
