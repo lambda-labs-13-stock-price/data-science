1c70e524b83ba4ab34c3e2ddfc5a87f98e92563c
Ruwai <chrisl6uie@gmail.com>
2019-06-13 13:07:01 -0700

    first commit, added some example glue jobs. literally just reading from data catalogged database (might not actually work this would require our database to be in the Glue Data Catalog) need to write a script that dumps from DB to CSV without the need of Glue

---
+++ src/preprocessing/glue/twitter_etl.py
@@ -0,0 +1,19 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.dynamicframe import DynamicFrame
+from awsglue.job import Job
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf
+from pyspark.sql.types import StringType
+
+glueContext = GlueContext(SparkContext.getOrCreate())
+DB = "RDS_tweets"
+TABLE = "tweets"
+input_dir = "http://s3.us-west-2.amazonaws.com/hiddenalphabet/datasets"
+output_dir = "http://s3.us-west-2.amazonaws.com/hiddenalphabet/transformed_datasets"
+
+tweets_dyf = glueContext.create_dynamic_frame.from_catalog(database=DB, table_name=TABLE)
+tweets_df = tweets_dyf.toDF()
