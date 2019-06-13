31df00ca7cefcfa3e4a8ea6ecd2f1efea5ff6146
Cole Hudson <cole@colejhudson.com>
2019-06-13 11:12:45 -0700

    removed credentials to database

--- src/api/sentiment-analysis/app.py
+++ src/api/sentiment-analysis/app.py
@@ -9,31 +9,37 @@ app = Flask(__name__)
 
 TICKERS_TO_COMPANY = json.loads(open('tickers.json').read())
 
+PG_USER = os.environ['PG_USER']
+PG_PASS = os.environ['PG_PASS']
+PG_HOST = os.environ['PG_HOST']
+PG_PORT = os.environ['PG_PORT']
+
 @app.route('/', methods=['POST'])
 def main():
-   engine = db.create_engine('postgresql+psycopg2://HIDDENALPHABET:password@hiddenalphabet-db.ceqxyonbodui.us-east-1.rds.amazonaws.com:5432/HiddenAlphabet')
-   connection = engine.connect()
-   metadata = db.MetaData()
-   comments = db.Table('reddit-comments', metadata, autoload=True, autoload_with=engine)
+    url = 'postgresql://'+PG_USER+':'+PG_PASS+'@'+PG_HOST+':'+PG_PORT+'/HiddenAlphabet'
+    engine = db.create_engine(url)
+    connection = engine.connect()
+    metadata = db.MetaData()
+    comments = db.Table('reddit-comments', metadata, autoload=True, autoload_with=engine)
 
-   query = db.select([comments])
-   cursor = connection.execute(query)
-   rows = cursor.fetchall()
+    query = db.select([comments])
+    cursor = connection.execute(query)
+    rows = cursor.fetchall()
 
-   corpus = pd.DataFrame(rows)
-   corpus.columns = rows[0].keys()
-   json = request.get_json(force=True)
+    corpus = pd.DataFrame(rows)
+    corpus.columns = rows[0].keys()
+    json = request.get_json(force=True)
 
-   ticker = json['query']      
+    ticker = json['query']      
 
-   lowercase = CORPUS['text'].str.lower()
-   contains_name_masks = [lowercase.str.contains(company) for company in TICKERS_TO_COMPANY[ticker]]
-   any_comany_alias = reduce(lambda agg, curr: agg | curr, contains_name_masks)
+    lowercase = CORPUS['text'].str.lower()
+    contains_name_masks = [lowercase.str.contains(company) for company in TICKERS_TO_COMPANY[ticker]]
+    any_comany_alias = reduce(lambda agg, curr: agg | curr, contains_name_masks)
 
-   matching = corpus[any_company_alias]
-   average_score = matching['sentiment_score'].mean()
+    matching = corpus[any_company_alias]
+    average_score = matching['sentiment_score'].mean()
 
-   return jsonify(score=average_score)
+    return jsonify(score=average_score)
 
 if __name__ == '__main__':
     app.run()
