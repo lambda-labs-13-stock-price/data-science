c717a59cd9eaf8b79f38960476dc93716580e94a
Cole Hudson <cole@colejhudson.com>
2019-06-13 11:25:13 -0700

    removed credentials to database

--- src/api/datetime-score/app.py
+++ src/api/datetime-score/app.py
@@ -3,21 +3,23 @@ import sqlalchemy as db
 import pandas as pd
 import random
 import json
+import os
 
 app = Flask(__name__)
 
-TICKERS = json.loads(open('tickers.json').read())
+PG_USER = os.environ['PG_USER']
+PG_PASS = os.environ['PG_PASS']
+PG_HOST = os.environ['PG_HOST']
+PG_PORT = os.environ['PG_PORT']
 
 @app.route('/', methods=['POST'])
 def main(search):
    engine = db.create_engine('postgresql+psycopg2://HIDDENALPHABET:password@hiddenalphabet-db.ceqxyonbodui.us-east-1.rds.amazonaws.com:5432/HiddenAlphabet')
    connection = engine.connect()
 
-   metadata = db.MetaData()
-   comments = db.Table('reddit-comments', metadata, autoload=True, autoload_with=engine)
-   query = db.select([comments])
-   cursor = connection.execute(query)
-   rows = cursor.fetchall()
+    url = 'postgresql://'+PG_USER+':'+PG_PASS+'@'+PG_HOST+':'+PG_PORT+'/HiddenAlphabet'
+    engine = db.create_engine(url)
+    connection = engine.connect()
 
    corpus = pd.DataFrame(rows)
    corpus.columns = rows[0].keys()
