03e4d93520783350ec758300cb705d18824e3e41
Cole Hudson <cole@colejhudson.com>
2019-06-14 01:13:04 -0700

    added deploy target to makefile

--- scripts/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-aws iam create-policy \
-    --policy-name USWest2AdministratorAccessOnly \
-    --description "Provides full access in us-west-2 (Oregon) only"\
-    --policy-document file://config/us-west-2-deployments-only-policy.json
