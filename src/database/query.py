a4a1b2a1e0204d8b0fcb9fc21aa112b374a20699
Ruwai <chrisl6uie@gmail.com>
2019-06-15 09:40:22 -0700

    added query redditcomments to query.py

--- src/database/query.py
+++ src/database/query.py
@@ -1,8 +1,15 @@
 from sqlalchemy.sql import text
-from tables import Tweets
+from tables import Tweets, RedditComments
 
 def all_tweets(session):
     return session.query(Tweets).all()
 
 def tweets_between(session, start, finish):
     return session.query(Tweets).filter(start >= Tweets.time & Tweets.time <= finish)
+
+def all_reddit_comments(session):
+    return session.query(RedditComments).all()
+
+def reddit_comments_between(session, start, finish):
+    return session.query(RedditComments).filter(start >= RedditComments.time & RedditComments.time <= finish)
+
